05b47f14f9d5973cb17fe964fc64a256e8f3c898
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-11-02 19:12:55 +0200

    Updates

---
+++ precache-manifest.e777943439577bcb732254054c970ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362047e9df48e89225405a21e50e02a5",
+    "url": "/calcwin/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calcwin/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "dd8bb3ea3a3c19b9912c",
+    "url": "/calcwin/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/calcwin/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "dd8bb3ea3a3c19b9912c",
+    "url": "/calcwin/static/js/main.3fa8d32d.chunk.js"
+  },
+  {
+    "revision": "191d7c25a77162f2030e",
+    "url": "/calcwin/static/js/runtime~main.7a8a51b9.js"
+  }
+]);
